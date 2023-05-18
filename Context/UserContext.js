c911e28b49756ac5aec7bbada600a7f0770205e3
Desktop 1 <dzo242@gmail.com>
2023-05-18 23:30:29 +0200

    pour

--- Context/UserContext.js
+++ Context/UserContext.js
@@ -1,23 +1,17 @@
 import React, {useState, createContext} from 'react';
-import { useContext } from 'react';
-import { signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 
 const UserContext = createContext([{}, () => {}]);
 
-const UserProvider = (children) => {
-    return createUserWithEmailAndPassword(email, password)
+const UserProvider = (props) => {
     const [state, setState] = useState({
         username: "",
         email: "",
         uid: "",
         isLoggedIn: null,
+        profilePhotoUrl: "default",
     });
 
-    return <UserContext.Provider value={[state, setState]}>{children}</UserContext.Provider>
+    return <UserContext.Provider value={[state, setState]}>{props.children}</UserContext.Provider>
 };
 
-function useUserAuth() {
-    return useContext(UserContext);
-}
-
-export { UserContext, UserProvider, useUserAuth };
+export { UserContext, UserProvider };
