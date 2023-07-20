f12344560cf335a3fa69dcba578ae9d00a494eb5
Desktop 1 <dzo242@gmail.com>
2023-07-20 22:04:37 +0200

    e

--- App.js
+++ App.js
@@ -9,13 +9,7 @@ import { store } from './redux/store/index';
 import { ThemeProvider } from './Context/ThemeContext';
 
 const publishableKey = 'pk_live_51NHsDFIldimfBY6sVhbIjpv4YRu5srhfFolF3tDpO2MorXH7qk7RpLx0MMsMmQURyRLOKJkIXwrcfLTRymUcWq8G00GFwgRXEW';
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://airlibre-9c426.firebaseio.com'
-});
 
 export default function App() {
   return (
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet, Text, View, Dimensions, RefreshControl, TouchableOpacity, FlatList, ScrollView, Image,  Pressable } from 'react-native';
-import * as Animatable from 'react-native-animatable';
-import SearchComponent from '../components/SearchComponent';
-import { UserContext } from '../Context/UserContext';
 import { Ionicons } from '@expo/vector-icons';
-import { ThemeContext } from '../Context/ThemeContext';
 import { AntDesign } from '@expo/vector-icons';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import firebase from 'firebase/compat/app';
+import { ThemeContext } from '../Context/ThemeContext';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 require('firebase/compat/firestore');
@@ -39,8 +36,6 @@ const HomeScreen = ({ navigation }) => {
     });
   }, []);
 
-
-
   const getPosts = async () => {
     setLoading(true);
     const snapshot = await refPosts.orderBy('title', 'desc').limit(10).get();
@@ -67,75 +62,59 @@ const HomeScreen = ({ navigation }) => {
     setIsRefreshing(false);
   };
 
-  const animation = {
-    from: {
-      opacity: 0,
-      translateY: 90,
-    },
-    to: {
-      opacity: 1,
-      translateY: 0,
-    },
-  };
+  const renderItem = ({ item }) => (
+    <View style={[styles.item, isDarkMode && styles.itemDark]}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <View style={styles.infoContainer}>
+        <Text style={[styles.title, isDarkMode && styles.titleDark]}>{item.title}</Text>
+        <Text style={[styles.category, isDarkMode && styles.categoryDark]}>{item.categorie}</Text>
+        <Text style={[styles.price, isDarkMode && styles.priceDark]}>{item.prix}€</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => {
+          navigation.navigate('Detail', {
+            item: item,
+          });
+        }}
+      >
+        <Text style={styles.addButtonText}>Détails</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
     <View style={[styles.container, isDarkMode && styles.containerDark]}>
-      <View style={styles.subheader}>
-        <View style={styles.headerLeft}>
-          <TouchableOpacity style={styles.toggleButton} onPress={toggleDarkMode}>
-            <Ionicons name={isDarkMode ? 'md-sunny' : 'md-moon'} size={24} color={isDarkMode ? '#FFF' : '#000'} />
-          </TouchableOpacity>
-        </View>
-        <View style={styles.headerCenter}>
-          <Text style={[styles.hi, isDarkMode && styles.hiDark]}>Bonjour, {username}</Text>
-        </View>
-        <View style={styles.headerRight}>
-          <TouchableOpacity style={styles.search} onPress={navigation.navigate('SearchScreen')}>
-              <AntDesign name="search1" size={24} color={isDarkMode ? '#FFF' : '#000'} />
-          </TouchableOpacity>
-        </View>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.toggleButton} onPress={toggleDarkMode}>
+          <Ionicons name={isDarkMode ? 'md-sunny' : 'md-moon'} size={24} color={isDarkMode ? '#FFF' : '#000'} />
+        </TouchableOpacity>
+        <Text style={[styles.hi, isDarkMode && styles.hiDark]}>Bonjour, {username}</Text>
+        <TouchableOpacity style={styles.search} onPress={navigation.navigate('Search')}>
+          <AntDesign name="search1" size={24} color={isDarkMode ? '#FFF' : '#000'} />
+        </TouchableOpacity>
       </View>
-      <View style={styles.main}>
-        <Text style={[styles.recommand, isDarkMode && styles.recommandDark]}>Nos Recommandations</Text>
+      <View style={styles.recommendationContainer}>
+        <Text style={[styles.recommendation, isDarkMode && styles.recommendationDark]}>Nos Recommandations</Text>
       </View>
       <FlatList
         columnWrapperStyle={{ justifyContent: 'space-between' }}
         data={post}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{
-          marginTop: 50,
+          paddingHorizontal: 20,
           paddingBottom: 50,
-          marginBottom: 55,
         }}
         keyExtractor={(item) => item.id}
         numColumns={2}
-        refreshControl={
-          <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} tintColor="#F8852D" />
-        }
+        refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} tintColor="#F8852D" />}
         extraData={selectedId}
-        renderItem={({ item }) => (
-          <View>
-            <Pressable
-              style={[styles.item, isDarkMode && styles.itemDark]}
-              onPress={() => {
-                navigation.navigate('Detail', {
-                  item: item,
-                });
-              }}>
-              <Image source={{ uri: item.image }} style={{ width: 150, height: 150, borderRadius: 15 }} />
-              <Text style={[styles.title, isDarkMode && styles.titleDark]}>{item.title}</Text>
-              <Text style={[styles.text, isDarkMode && styles.textDark]}>{item.categorie}</Text>
-              <Text style={[styles.text, styles.price, isDarkMode && styles.textDark]}>{item.prix}€</Text>
-            </Pressable>
-          </View>
-        )}
+        renderItem={renderItem}
       />
     </View>
   );
-  
 };
 
-const width = Dimensions.get('screen').width / 2;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -144,16 +123,7 @@ const styles = StyleSheet.create({
   containerDark: {
     backgroundColor: '#000',
   },
-  hi: {
-    fontSize: 18,
-    color: '#000',
-    fontWeight: 'bold',
-    marginBottom: 30,
-  },
- hiDark: {
-    color: '#fff',
-  },
-  subheader: {
+  header: {
     marginTop: 30,
     paddingHorizontal: 20,
     paddingVertical: 20,
@@ -161,80 +131,90 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  headerLeft: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  hi: {
+    fontSize: 18,
+    color: '#000',
+    fontWeight: 'bold',
   },
-  headerCenter: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  hiDark: {
+    color: '#fff',
   },
   toggleButton: {
     borderRadius: 25,
     paddingVertical: 10,
     paddingHorizontal: 10,
-    marginTop: -27,
+    backgroundColor: '#fff',
   },
   search: {
-    marginTop: -27,
+    borderRadius: 25,
     paddingVertical: 10,
     paddingHorizontal: 10,
+    backgroundColor: '#fff',
   },
-  toggleButtonDark: {
-    backgroundColor: '#000',
-  },
-  toggleButtonText: {
-    color: '#000',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  toggleButtonTextDark: {
-    color: '#f7f7f7',
-  },
-  main: {
-    width: '100%',
-    marginTop: 55,
+  recommendationContainer: {
+    marginTop: 10,
     paddingHorizontal: 20,
   },
-  recommand: {
+  recommendation: {
     fontSize: 20,
     color: 'black',
     fontWeight: 'bold',
   },
-  recommandDark: {
+  recommendationDark: {
     color: '#fff',
   },
   item: {
-    height: 220,
-    width: width,
-    fontSize: 12,
-    fontWeight: 'bold',
-    elevation: 10,
-    marginHorizontal: 2,
+    height: 260,
+    width: '48%',
     borderRadius: 10,
     marginBottom: 20,
-    padding: 15,
+    padding: 10,
+    backgroundColor: '#fff',
+    elevation: 5,
+    justifyContent: 'space-between',
   },
   itemDark: {
     backgroundColor: '#333',
   },
+  image: {
+    width: '100%',
+    height: 150,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  infoContainer: {},
   title: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: 'bold',
+    color: '#000',
   },
   titleDark: {
     color: '#fff',
   },
-  text: {
-    fontSize: 10,
-    paddingHorizontal: 1,
+  category: {
+    fontSize: 12,
     color: '#848385',
+    marginBottom: 5,
   },
-  textDark: {
+  categoryDark: {
     color: '#ccc',
   },
   price: {
-    marginTop: 5,
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  priceDark: {
+    color: '#fff',
+  },
+  addButton: {
+    backgroundColor: '#F8852D',
+    borderRadius: 25,
+    paddingVertical: 8,
+    alignItems: 'center',
+  },
+  addButtonText: {
+    color: '#fff',
     fontWeight: 'bold',
   },
 });
