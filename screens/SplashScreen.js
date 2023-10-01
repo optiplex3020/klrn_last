8b3ecf8c01fb37b04ef8f05aee985dc8256c85a7
optiplex3020 <dzo242@gmail.com>
2023-10-01 12:37:41 +0200

    LAST

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,99 +1,68 @@
-import React, { useEffect, useRef } from 'react'
-import { Animated, Dimensions, Image, Text, View } from "react-native";
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import {LinearGradient} from 'expo-linear-gradient';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, Animated, Easing, Dimensions } from 'react-native';
 
 export default function SplashScreen({ navigation }) {
+  const startAnimation = useRef(new Animated.Value(0)).current;
+  const contentTransition = useRef(new Animated.Value(Dimensions.get('window').height)).current;
+  const opacityValue = useRef(new Animated.Value(0)).current;
+  const scaleValue = useRef(new Animated.Value(1)).current; // Utilisez une échelle pour l'animation de la taille du texte
+  const [showText, setShowText] = useState(false);
 
-    const edges = useSafeAreaInsets();
+  useEffect(() => {
+    Animated.timing(opacityValue, {
+      toValue: 10,
+      duration: 100,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
 
-    const startAnimation = useRef(new Animated.Value(0)).current;
-
-    const scaleLogo = useRef(new Animated.Value(1)).current;
-    const scaleTitle = useRef(new Animated.Value(1)).current;
-
-    // Offset Animation....
-    const moveLogo = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
-    const moveTitle = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
-
-
-    const contentTransition = useRef(new Animated.Value(Dimensions.get('window').height)).current;
-
-    useEffect(() => {
-        setTimeout(() => {
-          navigation.navigate('First');
-        }, 5000);
-      }, []);
+    setTimeout(() => {
+      setShowText(true);
+      Animated.timing(scaleValue, {
+        toValue: 10, // Taille finale du texte (ajustez selon vos besoins)
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: false,
+      }).start(() => {
+        navigation.navigate('Login');
+      });
+    }, 5000);
+  }, []);
 
   return (
-    <LinearGradient
-      colors={['#000', 'transparent']} // Dégradé de noir à transparent
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-    >
-        <View style={{
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
+    <View style={{
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+    }}>
+      <Animated.View style={{
+        flex: 1,
+        backgroundColor: "#000",
+        zIndex: 1,
+        transform: [
+          { translateY: startAnimation }
+        ]
+      }}>
+        <Animated.View style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
         }}>
-            <Animated.View style={{
-                flex: 1,
-                backgroundColor: "#000",
-                zIndex: 1,
-                transform: [
-                    { translateY: startAnimation }
-                ]
-            }}>
-
-                <Animated.View style={{
-                    flex: 1,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                    <Animated.Image source={"../assets/sh.png"} style={{
-                        width: 100,
-                        height: 100,
-                        marginBottom: 20,
-                        transform: [
-                            { translateX: moveLogo.x },
-                            { translateY: moveLogo.y },
-                            { scale: scaleLogo },
-
-                        ]
-                    }}></Animated.Image>
-
-                    <Animated.Text style={{
-                        fontSize: 25,
-                        fontWeight: 'bold',
-                        color: 'white',
-                        transform: [
-                            { translateY: moveTitle.y },
-                            { scale: scaleTitle }
-                        ]
-                    }}>Kolia</Animated.Text>
-
-                </Animated.View>
-
-            </Animated.View>
-
-            <Animated.View style={{
-                position: 'absolute',
-                top: 0,
-                bottom: 0,
-                left: 0,
-                right: 0,
-                backgroundColor: 'rgba(0,0,0,0.04)',
-                zIndex: 0,
-                transform: [
-                    { translateY: contentTransition }
-                ]
-            }}>
-
-            </Animated.View>
-
-        </View>
-
-    </LinearGradient>
+          {showText && (
+            <Animated.Text
+              style={{
+                color: 'white',
+                transform: [{ scale: scaleValue }], // Utilisez l'échelle pour l'animation de la taille
+                opacity: opacityValue,
+              }}
+            >
+              Kolia
+            </Animated.Text>
+          )}
+        </Animated.View>
+      </Animated.View>
+    </View>
   )
 }
