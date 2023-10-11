357b1c2d6da9830c6856219ae046612d99963abf
Desktop 1 <dzo242@gmail.com>
2023-10-11 18:07:23 +0200

    maj

--- payment_functions/index.js
+++ payment_functions/index.js
@@ -1,43 +1,34 @@
 const functions = require("firebase-functions");
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const admin = require("firebase-admin");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+admin.initializeApp();
 
 exports.createPaymentIntent = functions.https.onRequest(async (req, res) => {
-  // Check if the request is a POST request
-  if (req.method !== "POST") {
-    res.status(405).send("Only POST requests are supported");
-    return;
-  }
-
-  // Parse the request body
-  const body = req.body; // No need for .json() here
-
-  // Check if the request body contains the necessary information
-  if (!body.amount || !body.currency || !body.cardToken) {
-    res.status(400).send("Invalid request body");
-    return;
-  }
-
-  // Calculate the total price
-  const cartItems = body.cartItems;
-  const totalPrice = cartItems.reduce((total, item) => {
-    const itemTotalPrice = item.prix * item.quantity;
-    return total + itemTotalPrice;
-  }, 0);
-
-  // Log the totalPrice for debugging purposes
-  console.log("Total Price:", totalPrice);
-
-  // Make a payment with Stripe
   try {
-    const payment = await stripe.charges.create({
-      amount: totalPrice,
+    // Vérifiez que la demande est une requête POST
+    if (req.method !== "POST") {
+      res.status(405).send("Only POST requests are supported");
+      return;
+    }
+
+    // Parsez le corps de la demande
+    const body = JSON.parse(req.body);
+
+    // Vérifiez que le corps de la demande contient les informations nécessaires
+    if (!body.amount || !body.currency || !body.cardToken) {
+      res.status(400).send("Invalid request body");
+      return;
+    }
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: body.amount,
       currency: body.currency,
-      source: body.cardToken, // Use cardToken instead of card details
-      description: "Payment for cart items",
+      payment_method: body.cardToken,
+      confirm: true,
     });
 
-    // Send the response
-    res.status(200).send(payment);
+    res.status(200).json({clientSecret: paymentIntent.client_secret});
   } catch (error) {
     console.error("Stripe payment error:", error);
     res.status(500).send("Payment failed");
